1c3adb98810c239effe90498f0524427878ed28c
Calebfonyuy <fonyuyca@gmail.com>
2022-01-22 13:18:46 +0100

    Added all resource controllers for all models

---
+++ app/Http/Controllers/ResponsableGroupeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResponsableGroupe;
+use Illuminate\Http\Request;
+
+class ResponsableGroupeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ResponsableGroupe  $responsableGroupe
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ResponsableGroupe $responsableGroupe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ResponsableGroupe  $responsableGroupe
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ResponsableGroupe $responsableGroupe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ResponsableGroupe  $responsableGroupe
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ResponsableGroupe $responsableGroupe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ResponsableGroupe  $responsableGroupe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ResponsableGroupe $responsableGroupe)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ResponsableSousZoneController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResponsableSousZone;
+use Illuminate\Http\Request;
+
+class ResponsableSousZoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ResponsableSousZone  $responsableSousZone
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ResponsableSousZone $responsableSousZone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ResponsableSousZone  $responsableSousZone
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ResponsableSousZone $responsableSousZone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ResponsableSousZone  $responsableSousZone
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ResponsableSousZone $responsableSousZone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ResponsableSousZone  $responsableSousZone
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ResponsableSousZone $responsableSousZone)
+    {
+        //
+    }
+}
