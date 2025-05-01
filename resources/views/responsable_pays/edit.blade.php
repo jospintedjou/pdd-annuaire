3351cc0959f997e47888cc53b4e7a97cf635ad8f
jospin <tedjoujos910gmail.com>
2025-05-01 17:40:32 +0100

    Pays model added with CRUD, other model like Sous-zone ans user adapted, responsable_pays also added

--- resources/views/responsable_pays/edit.blade.php
+++ resources/views/responsable_pays/edit.blade.php
@@ -46,7 +46,7 @@
                                     </div>
 
                                     <div class="row">
-                                        @dd($responsabilites)
+                                    
                                     @foreach($responsabilites as $responsabilite)
                                         <div class="col-md-6">
                                             <div class="form-group @error('responsabilite_pays') has-danger @enderror">
